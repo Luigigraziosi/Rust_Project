ecf7a0289d5f9883be4c9efbb9aeb4dcdf27daf3
Luigigraziosi <lgraziosi38@gmail.com>
2024-03-18 13:58:54 +0100

    until 6

---
+++ lab0/movesemantics/src/main.rs
@@ -0,0 +1,104 @@
+/*
+fn main() {
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec(vec0);
+
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+}
+
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec1 = vec;
+
+    vec1.push(88);
+    for i in 0..vec1.len(){
+        println!("{}", vec1[i]);
+    }
+    //println!("{:?}", vec1);
+    vec1
+}
+ */
+/* fn main() {
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec(vec0.clone());
+
+    assert_eq!(vec0, vec![22, 44, 66]);
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+    println!("{:?}", vec0);
+    println!("{:?}", vec1);
+}
+
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(88);
+
+    vec
+}*/
+/* fn main() {
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec( vec0.clone());
+
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+    println!("{:?}", vec1);
+
+}
+
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    vec.push(88);
+
+    vec
+}*/
+/* fn main() {
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec(vec0);
+
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+}
+
+// `fill_vec()` no longer takes `vec: Vec<i32>` as argument - don't change this!
+fn fill_vec() -> Vec<i32> {
+    // Instead, let's create and fill the Vec in here - how do you do that?
+    let mut vec = Vec:: new();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+    vec.push(88);
+
+    vec
+}
+
+*/
+/*fn main() {
+    let mut x = 100;
+    let y = &mut x;
+    *y += 100;
+    let z = &mut x;
+    *z += 1000;
+    assert_eq!(x, 1200);
+    println!("{}", x)
+} */
+/* */
+fn main() {
+    let data = "Rust is great!".to_string();
+
+    println!("{}", get_char(data));
+
+    string_uppercase(&data);
+}
+
+// Should not take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(data: &String) {
+    data = data.to_uppercase();
+
+    println!("{}", data);
+}
--- lab0/vecs/src/main.rs
+++ lab0/vecs/src/main.rs
@@ -36,7 +36,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
 fn test_vec_loop() {
     let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
     let ans = vec_loop(v.clone());
-
+-
     assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
 }
 
